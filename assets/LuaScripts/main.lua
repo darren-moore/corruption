a69b49cc79cce78115417cb0bc27f560550bedc1
Darren <moodarren@gmail.com>
2019-11-30 00:02:41 -0500

    Pass FSM to enemy constructor

--- assets/LuaScripts/main.lua
+++ assets/LuaScripts/main.lua
@@ -9,6 +9,20 @@ startTime = 0
 
 backgroundData = {velocity = 75, acceleration = 0, accelerating = true}
 
+function FSM(SB, EB)
+    local fsm = machine.create({
+        initial = 'sb',
+        events = {
+          { name = 'toEB',  from = 'sb',  to = 'eb' }}, 
+        update_functions = {
+          duringsb = SB, 
+          duringeb = EB,
+        }
+    })
+
+    return fsm
+end
+
 function BindOptions(f, options)
     return function(enemy, dt)
         return f(options, enemy, dt)
@@ -19,24 +33,14 @@ function MakePatternData(pattern, delay, freq)
     return {pattern = pattern, delay = delay, freq = freq}
 end
 
-function AddEnemy(spawnTime, SB, SB_length, EB, spawnPosition, patternData, health)
+function AddEnemy(spawnTime, fsm, SB_length, spawnPosition, patternData, health)
     local enemy = {}
     enemy.spawnTime = spawnTime
-    enemy.SB = SB
     enemy.transitionTime = spawnTime + SB_length
-    enemy.EB = EB
     enemy.pos = spawnPosition
     enemy.patternData = patternData
     enemy.fireTime = patternData.delay
-    enemy.fsm = machine.create({
-        initial = 'sb',
-        events = {
-          { name = 'toEB',  from = 'sb',  to = 'eb' }}, 
-        update_functions = {
-          duringsb = function(enemy, dt) SB(enemy, dt) end, 
-          duringeb = function(enemy, dt) EB(enemy, dt) end,
-        }
-    })
+    enemy.fsm = fsm
 
     if health == nil then
         health = 5
@@ -86,122 +90,17 @@ function _Init()
         local SB = BindOptions(Behaviour_DampedMoveToTarget, {target = MakeVec2(100, 100), speed = 25})
         local EB = BindOptions(Behaviour_Nil)
         local pat = MakePatternData("enemy1.xml", .5, 2)
-        AddEnemy(st, SB, 10, EB, MakeVec2(100, -100), pat)
+        AddEnemy(st, FSM(SB, EB), 10, MakeVec2(100, -100), pat)
 
         st = st + 1
         SB = BindOptions(Behaviour_DampedMoveToTarget, {target = MakeVec2(300, 125), speed = 25})
-        AddEnemy(st, SB, 10, EB, MakeVec2(300, -150), pat)
+        AddEnemy(st, FSM(SB, EB), 10, MakeVec2(300, -150), pat)
 
         st = st + .5
         SB = BindOptions(Behaviour_DampedMoveToTarget, {target = MakeVec2(500, 150), speed = 25})
-        AddEnemy(st, SB, 10, EB, MakeVec2(500, -175), pat)
+        AddEnemy(st, FSM(SB, EB), 10, MakeVec2(500, -175), pat)
     end
 
-    st = st + 1.5
-
-    -- separated simple
-    do
-        local SB = BindOptions(Behaviour_DampedMoveToTarget, {target = MakeVec2(100, 100), speed = 15})
-        local EB = BindOptions(Behaviour_Nil)
-        local pat = MakePatternData("enemy1.xml", .5, 3)
-        AddEnemy(st, SB, 10, EB, MakeVec2(-50, 250), pat)
-
-        SB = BindOptions(Behaviour_DampedMoveToTarget, {target = MakeVec2(700, 100), speed = 15})
-        AddEnemy(st, SB, 10, EB, MakeVec2(850, 250), pat)
-    end
-
-    st = st + 4
-
-    -- nil group 1
-    do
-        local SB = BindOptions(Behaviour_MoveAcross, {direction = 100, speed = 250})
-        local EB = BindOptions(Behaviour_Kill)
-        AddEnemy(st, SB, 15, EB, MakeVec2(600, -50), nilpat)
-        AddEnemy(st, SB, 15, EB, MakeVec2(650, -50), nilpat)
-        local SB = BindOptions(Behaviour_MoveAcross, {direction = 110, speed = 245})
-        AddEnemy(st, SB, 15, EB, MakeVec2(600, -50), nilpat)
-        AddEnemy(st, SB, 15, EB, MakeVec2(700, -50), nilpat)
-        local SB = BindOptions(Behaviour_MoveAcross, {direction = 107, speed = 255})
-        AddEnemy(st, SB, 15, EB, MakeVec2(600, -50), nilpat)
-    end
-
-    st = st + 1
-
-    -- nil group 2
-    do
-        local SB = BindOptions(Behaviour_MoveAcross, {direction = 80, speed = 250})
-        local EB = BindOptions(Behaviour_Kill)
-        AddEnemy(st, SB, 15, EB, MakeVec2(200, -50), nilpat)
-        AddEnemy(st, SB, 15, EB, MakeVec2(250, -50), nilpat)
-        local SB = BindOptions(Behaviour_MoveAcross, {direction = 70, speed = 245})
-        AddEnemy(st, SB, 15, EB, MakeVec2(200, -50), nilpat)
-        AddEnemy(st, SB, 15, EB, MakeVec2(300, -50), nilpat)
-        local SB = BindOptions(Behaviour_MoveAcross, {direction = 75, speed = 255})
-        AddEnemy(st, SB, 15, EB, MakeVec2(200, -50), nilpat)
-    end
-
-    st = st + 3
-
-    -- big guy + "distractions" on side
-    do
-        
-        local EB = BindOptions(Behaviour_MoveAcross, {direction = 200, speed = 100})
-        local SB = BindOptions(Behaviour_DampedMoveToTarget, {target = MakeVec2(400, 150), speed = 15})
-        local pat = MakePatternData("Waves.xml", 1, 2)
-        AddEnemy(st, SB, 5, EB, MakeVec2(400, -100), pat, 170)  
-        
-        st = st + .5
-
-        local SB = BindOptions(Behaviour_DampedMoveToTarget, {target = MakeVec2(100, 100), speed = 15})
-        local pat = MakePatternData("enemy1.xml", .5, 3)
-        AddEnemy(st, SB, 10, EB, MakeVec2(-50, 250), pat)
-
-        SB = BindOptions(Behaviour_DampedMoveToTarget, {target = MakeVec2(700, 100), speed = 15})
-        AddEnemy(st, SB, 10, EB, MakeVec2(850, 250), pat)
-    end
-
-    st = st + 20
-
-    -- speed up enemies
-    -- "1.. 1.2.. .. 1.2.3."
-    do
-        local SB = BindOptions(Behaviour_MoveAcross, {direction = -90, speed = 1200})
-        local EB = BindOptions(Behaviour_Kill)
-        AddEnemy(st, SB, 1, EB, MakeVec2(300, 700), nilpat, 500)
-        st = st + .5
-
-        AddEnemy(st, SB, 1, EB, MakeVec2(300, 700), nilpat, 500)
-        st = st + .1
-        AddEnemy(st, SB, 1, EB, MakeVec2(250, 700), nilpat, 500)
-        st = st + 1
-
-        AddEnemy(st, SB, 1, EB, MakeVec2(500, 700), nilpat, 500)
-        st = st + .075
-        AddEnemy(st, SB, 1, EB, MakeVec2(550, 700), nilpat, 500)
-        st = st + .1
-        AddEnemy(st, SB, 1, EB, MakeVec2(520, 700), nilpat, 500)
-        st = st + 1
-        SB = BindOptions(Behaviour_SpeedUpPace)
-        AddEnemy(st, SB, 10, EB, MakeVec2(-100, -100), nilpat, 500)
-    end
-
-    st = st + 5
-    
-    -- reintroduce enemies
-    do
-        local SB = BindOptions(Behaviour_DampedMoveToTarget, {target = MakeVec2(100, 100), speed = 25})
-        local EB = BindOptions(Behaviour_Nil)
-        local pat = MakePatternData("enemy1.xml", .5, 2)
-        AddEnemy(st, SB, 10, EB, MakeVec2(100, -100), pat)
-
-        st = st + 1
-        SB = BindOptions(Behaviour_DampedMoveToTarget, {target = MakeVec2(300, 125), speed = 25})
-        AddEnemy(st, SB, 10, EB, MakeVec2(300, -150), pat)
-
-        st = st + .5
-        SB = BindOptions(Behaviour_DampedMoveToTarget, {target = MakeVec2(500, 150), speed = 25})
-        AddEnemy(st, SB, 10, EB, MakeVec2(500, -175), pat)
-    end
 end
 
 function _Cleanup()
